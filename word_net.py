135b3c19fd9519be727d757efaf2510baa2ec7bc
Anurag Kumar <anuragkumarak95@gmail.com>
2017-09-14 22:19:21 +0530

    updated word_net.py,now dumping 2 arrays as resulting network. added journal file for personal use.

--- word_net.py
+++ word_net.py
@@ -4,6 +4,11 @@ Module for creating a network of word entities(look @ models/word.py for entity 
 '''
 from models.word import Word
 import pickle, sys, time
+from bin.paint import paint
+
+# file_path for dumping data
+dump_file = 'test/word_net.wrdnt'
+TAG = paint('WORDNET/','b')
 def generate_net(idf,tf_idf):
     word_net = {} # list of word entities.
     
@@ -17,10 +22,23 @@ def generate_net(idf,tf_idf):
         for word in docs:
             word_net[word].addtofrwrd_links([w if w != word else None for w in docs])
 
+    words = {}
+    words_arr = []
+    relatives = []
+    i=0
+    for k,word in word_net.items():
+        words_arr.append(k)
+        words[k] = i
+        i+=1
+    total = len(words)
+    i=0 # % counter var
     for _,word in word_net.items():
-        if len(word.frwrd_links) != 0:
-            print(word.w,' :: ',len(word.frwrd_links))
-    pickle.dump(word_net,open('word_net.wrdnt','wb'),protocol=pickle.HIGHEST_PROTOCOL)
+        i+=1
+        print('\r'+TAG+' '+paint(str((i/total)*100),'r')+'% completed...',end='')    
+        relatives.append([words[w] if w else None for w in word.frwrd_links]) 
+    print()
+    pickle.dump((words_arr,relatives),open(dump_file,'wb'),protocol=pickle.HIGHEST_PROTOCOL)
+    print(TAG,'word network dumped @',dump_file)
 
 if __name__=='__main__':
     idf,tf_idf = pickle.load(open('loadouts/loadout.tfidfpkl','rb'))
