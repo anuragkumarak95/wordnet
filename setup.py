c8ff04fbb53312c60bb9a5f5e8b922444e3b2ef7
Anurag Kumar <anuragkumarak95@gmail.com>
2017-09-17 01:45:49 +0530

    error handled for setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
 from setuptools import setup
-import pypandoc
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
